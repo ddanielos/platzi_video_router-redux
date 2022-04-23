c8b40c5c5b214be7583b25b7eb6d4839a3e06259
dany-dev <dany.oblitas1984@gmail.com>
2022-04-23 11:58:51 -0500

    finalizado curso práctico de react

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import React from "react";
 //import ReactDOM from "react-dom"; es reemplazado en nuevas versiones por:
 import { createRoot } from "react-dom/client"
 //-------------------------------------------------------------------------
-
-import HelloWorld from "./components/HelloWorld"
+import App from "./containers/App";
 
 //ReactDOM.render( <HelloWorld/>, document.getElementById('app'));
 //Reemplazado en nuevas versiones por:
 const root = createRoot(document.getElementById('app'))
-root.render(<HelloWorld />)
+root.render(<App />)
